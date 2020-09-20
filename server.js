bbea4631479bb3879e12d1f529d89674bd77754f
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2020-09-20 12:11:54 -0700

    server running with connection to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
+const mongoose = require("mongoose");
 const { mongoUser, mongoPw, mongoName } = require("./config");
 
 const index = require("./routes/index");
@@ -14,6 +13,18 @@ app.use(BodyParser.json()); // for parsing application/json
 app.use(BodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use("/index", index);
-app.listen(port, () => console.log(`Server is running on port: ${port}`));
+// app.listen(port, () => console.log(`Server is running on port: ${port}`));
 
 module.exports = app;
+
+mongoose
+   .connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.74paq.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
+   )
+   .then(() => {
+      app.listen(8080);
+      console.log("Server live on port 8080");
+   })
+   .catch((err) => {
+      console.log(err);
+   });
