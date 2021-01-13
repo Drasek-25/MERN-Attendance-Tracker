8efc2d983ca150a0825ce815c689fec25304ec3f
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2021-01-12 18:59:38 -0800

    added dropdown menu for classDate as DateAttendance component, added css, images, and transitions for these elements

--- client/src/components/ClassDate.js
+++ client/src/components/ClassDate.js
@@ -1,20 +1,57 @@
+import { MongooseDocument } from "mongoose";
 import { useState } from "react";
 import DateAttendance from "./DateAttendance";
 
 const ClassDate = ({ classDate }) => {
    const [active, setActive] = useState(false);
+   const [transition, setTransition] = useState(false);
 
    const handleClick = () => {
       setActive(!active);
+      setTimeout(() => setTransition(!transition), 1);
+   };
+   const handleDate = (isoString) => {
+      return new Date(isoString).toDateString();
    };
 
    return (
       <>
-         <div className="class-date" key={classDate._id} onClick={handleClick}>
-            <span className="class-date__text">{classDate.date}</span>
-            <button className="class-date__button">{active ? "-" : "+"}</button>
+         <div
+            className={
+               "class-date " +
+               (active ? "class-date--active" : "class-date--inactive")
+            }
+            key={classDate._id}
+            onClick={handleClick}
+         >
+            <span className="class-date__text">
+               {handleDate(classDate.date)}
+            </span>
+            <button
+               className={
+                  "class-date__button " +
+                  (active && "class-date__button--active")
+               }
+            >
+               {active ? "-" : "+"}
+            </button>
          </div>
-         {active && <DateAttendance students={classDate.students} />}
+         {active && (
+            <div
+               className={
+                  "date-attendance " + (transition && "date-attendance--active")
+               }
+            >
+               {classDate.students.map((student) => {
+                  return (
+                     <DateAttendance
+                        student={student}
+                        transition={transition}
+                     />
+                  );
+               })}
+            </div>
+         )}
       </>
    );
 };
--- client/src/components/DateAttendance.js
+++ client/src/components/DateAttendance.js
@@ -1,17 +1,42 @@
-const DateAttendance = ({ students }) => {
+import { useState } from "react";
+import greenCheck from "../data/images/green-checkmark-line.svg";
+import redXCheck from "../data/images/red-x-line.svg";
+
+const DateAttendance = ({ student, transition }) => {
+   const [inAttendance, setInAttendance] = useState(true);
+   const handleClick = () => {
+      setInAttendance(!inAttendance);
+   };
+
    return (
-      <div>
-         {students.map((student) => {
-            return (
-               <div>
-                  <button></button>
-                  <button></button>
-                  <span>{student.name}</span>
-               </div>
-            );
-         })}
+      <div
+         className={
+            "date-attendance__row " +
+            (transition && "date-attendance__row--active")
+         }
+      >
+         <img
+            src={greenCheck}
+            className={
+               "date-attendance__button " +
+               (inAttendance
+                  ? "date-attendance__button--active"
+                  : "date-attendance__button--inactive")
+            }
+            onClick={handleClick}
+         />
+         <img
+            src={redXCheck}
+            className={
+               "date-attendance__button " +
+               (!inAttendance
+                  ? "date-attendance__button--active"
+                  : "date-attendance__button--inactive")
+            }
+            onClick={handleClick}
+         />
+         <span>{student.name}</span>
       </div>
    );
 };
-
 export default DateAttendance;
